d59ddd3d8aca66a42382b0e7cc6b6614f74d4014
TweetyGuy <67609547+TweetyGuy@users.noreply.github.com>
2021-11-12 00:30:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function draw() {
+ var canvas = document.getElementById('canvas');
+ if(canvas.getContext) {
+   var c = canvas.getContext('2d');
+   var width = screen.width;
+   var height = screen.height;
+   
+   
+   c.fillStyle = 'rgba(200, 140, 15, 25)';
+   c.fillRect(width/2-15, height/2-30, 30, 60);
+ }
+}
